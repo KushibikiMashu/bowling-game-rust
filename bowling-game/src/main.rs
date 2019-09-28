e861e1b3f49482e5abc76c18b70a806032d80573
KushibikiMashu <kushibiki3413@gmail.com>
2019-09-28 15:17:00 +0900

    Add first test

--- bowling-game/src/main.rs
+++ bowling-game/src/main.rs
@@ -1,3 +1,34 @@
-fn main () {
+// all gutter
+// all one pin
+// one spare
+// one strike
+// all strike
+
+#[derive(Default)]
+struct Game {
+    score: u32
+}
+
+impl Game {
+    fn new() -> Self {
+        Game { score: 0 }
+    }
+
+    fn roll(&mut self, pins: u32) -> &mut Self {
+        self.score += pins;
+        self
+    }
+}
+
+fn main() {
     println!("Hello World");
+}
+
+mod tests {
+    use super::Game;
+
+    #[test]
+    fn all_gutter() {
+        Game::new().roll(0);
+    }
 }
